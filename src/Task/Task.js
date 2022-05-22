8fa08142aff438c9f1da4d61d0b846dabaa40c49
andrey678 <drew6496@yandex.ru>
2022-05-22 22:29:04 +0300

    Change jsx elements in Task component

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,21 +1,17 @@
-import './Task.scss';
-
 import { useDispatch, useSelector } from "react-redux";
+
+import './Task.scss';
 import { updateTask, inputTaskText } from '../redux/actions';
 
 
 function Task(props) {
     const { id, text } = props.data;
-    const modifiedText = useSelector(state =>state.todoTasksReducer.taskText);
+    const modifiedText = useSelector(state => state.todoTasksReducer.taskText);
     console.log(modifiedText);
     const dispatch = useDispatch();
 
 
- 
-    const handleChange = (event) => {
-        dispatch(inputTaskText(event.target.value));
-    }
-    const handleUpdate = (event) => {
+    const handleUpdate = () => {
         dispatch(updateTask(id, modifiedText));
     };
 
@@ -23,12 +19,11 @@ function Task(props) {
         <div className="task">
             <form onSubmit={handleUpdate} className='task__form' name="task">
                 <div className="task__block">
-                    <lable 
-                        className="task__label"
-                        onDblClick={handleChange}
-                        >
+                    <lable className="task__label">
                         <input type="checkbox" />
                         {text}
+                        <button type="button">Редактировать</button>
+                        <button type="button">Удалить</button>
                     </lable>
 
                     <input type="submit" hidden />
